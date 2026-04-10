6b2a0bb0e538870b97f062fc3efb1a16f81cda24
Harsh Rawat <kmsharshrawat@gmail.com>
2026-04-11 04:24:57 +0530

    render deploy error 4

--- backend/start-render.sh
+++ backend/start-render.sh
@@ -14,7 +14,6 @@ RUNTIME_HTTP_KEY="${NAKAMA_RUNTIME_HTTP_KEY:-change-me-http-key}"
 exec /nakama/nakama \
   --config /nakama/data/nakama-config.yml \
   --name nakama1 \
-  --runtime.js_entrypoint "index.js" \
   --socket.server_key "${SERVER_KEY}" \
   --session.encryption_key "${SESSION_ENC_KEY}" \
   --session.refresh_encryption_key "${SESSION_REFRESH_KEY}" \
